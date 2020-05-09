baebd3a8abe9ef8be408958dd15d8c1ab9a66030
uzumaki <narutouzumaki@github.com>
2020-05-09 20:58:06 +0530

    Section 2, Process 3

--- app/src/main/java/com/shivaay/msgsharingapp/MainActivity.kt
+++ app/src/main/java/com/shivaay/msgsharingapp/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.shivaay.msgsharingapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnShowToast.setOnClickListener {
+            //Code
+            Log.i("MainACtivity","Button was Clicked")
+            Toast.makeText( this,"Button was clicked",Toast.LENGTH_SHORT).show()
+        }
+
+        btnSendMessageToNextActivity.setOnClickListener {
+            val message = etUserMessage.text.toString()
+            Toast.makeText(this,message,Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(this,SecondActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
